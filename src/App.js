fc89a137f7d1c79a847633580f995df0f53e76a6
marsbars439 <marsbars439@gmail.com>
2025-11-19 09:54:51 -0500

    Unify dashboard dark theme styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AppProviders from './components/AppProviders';
 import './App.css';
 
 const MainLayout = ({ children }) => (
-  <div className="App">
+  <div className="App theme-dark">
     <div className="app-shell">
       <div className="layout-container">
         {children}
